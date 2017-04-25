b9e3cb1e30cabe6685692efffefcef8134b2f894
4hiziri <meirvg@gmail.com>
2017-04-25 13:15:25 +0900

    clean up code

--- aes.lisp
+++ aes.lisp
@@ -34,45 +34,6 @@
       (dotimes (j 4)
 	(setf (aref ret i j) (sub-byte (aref bytes-matrix i j) map))))))
 
-(defun shift-rows-map ()
-  "shift each 4bytes"
-  (let* ((len 128)
-	 (ret (make-array len))
-	 (c 0))
-    (loop repeat 4 for i from 0 by 32 do
-      (loop repeat 4
-	    for b-i from 0
-	    for j from 0 by 8 do
-	      (dotimes (n 8)
-		(setf (aref ret (mod (+ i j n (* b-i 32)) 128)) c)
-		(incf c))))
-    ret))
-
-(defparameter *shift-rows-map* #(0 1 2 3 4 5 6 7
-				 104 105 106 107 108 109 110 111
-				 80 81 82 83 84 85 86 87
-				 56 57 58 59 60 61 62 63
-				 32 33 34 35 36 37 38 39
-				 8 9 10 11 12 13 14 15
-				 112 113 114 115 116 117 118 119
-				 88 89 90 91 92 93 94 95
-				 64 65 66 67 68 69 70 71
-				 40 41 42 43 44 45 46 47
-				 16 17 18 19 20 21 22 23
-				 120 121 122 123 124 125 126 127
-				 96 97 98 99 100 101 102 103
-				 72 73 74 75 76 77 78 79
-				 48 49 50 51 52 53 54 55
-				 24 25 26 27 28 29 30 31))
-
-
-
-(defun shift-rows (bytes &optional (map *shift-rows-map*))
-  "shift each 4bytes"
-  (let ((ret (make-array 128 :element-type 'bit)))    
-    (dotimes (n 128 ret)
-      (setf (aref ret n) (aref bytes (aref map n))))))
-
 (defun shift-rows (bytes-mat)
   (dotimes (n 3 bytes-mat)
     (let ((i (1+ n)))
