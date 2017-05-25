89f129385e88e6d080504ad8efb4a67ac23896af
4hiziri <meirvg@gmail.com>
2017-05-25 09:47:16 +0900

    implement aes

--- aes.lisp
+++ aes.lisp
@@ -12,7 +12,7 @@
 
 (defparameter Nk 4)
 (defparameter Nb 4)
-(defparameter Nr 12)
+(defparameter Nr 10)
 
 (defparameter *subbyte-map*
   #(116 240 67 157 27 191 253 66 61 175 113 215 170 125 2 197 69 31 119 222 237
@@ -47,17 +47,59 @@
     (dotimes (i 8 (bit-xor (reverse ret) salt))
       (setf (aref ret i) (reduce (lambda (x y) (logxor x y))
 				 (bit-and byte (aref *matrix-sbox* i)))))))
-;; :TODO make hash-map
-(defun sub-bytes (state)
+
+;; sub-byte's input range is 0 <= input <= 255, so it turns into vector
+(defparameter *sub-byte-table*  
+  #(99 124 119 123 242 107 111 197 48 1 103 43 254 215 171 118 202 130 201 125
+    250 89 71 240 173 212 162 175 156 164 114 192 183 253 147 38 54 63 247 204 52
+    165 229 241 113 216 49 21 4 199 35 195 24 150 5 154 7 18 128 226 235 39 178
+    117 9 131 44 26 27 110 90 160 82 59 214 179 41 227 47 132 83 209 0 237 32 252
+    177 91 106 203 190 57 74 76 88 207 208 239 170 251 67 77 51 133 69 249 2 127
+    80 60 159 168 81 163 64 143 146 157 56 245 188 182 218 33 16 255 243 210 205
+    12 19 236 95 151 68 23 196 167 126 61 100 93 25 115 96 129 79 220 34 42 144
+    136 70 238 184 20 222 94 11 219 224 50 58 10 73 6 36 92 194 211 172 98 145
+    149 228 121 231 200 55 109 141 213 78 169 108 86 244 234 101 122 174 8 186
+    120 37 46 28 166 180 198 232 221 116 31 75 189 139 138 112 62 181 102 72 3
+    246 14 97 53 87 185 134 193 29 158 225 248 152 17 105 217 142 148 155 30 135
+    233 206 85 40 223 140 161 137 13 191 230 66 104 65 153 45 15 176 84 187 22))
+
+(defun loockup-byte-table (byte table)
+  (int2bit (aref table (bit2int byte)) 8))
+
+(defun sub-byte (byte)
+  (loockup-byte-table byte *sub-byte-table*))
+
+(defun map-state (state func)
   "substitude each byte in state."
-  (let ((ret (make-array (list 4 Nb))))    
+  (let ((ret (make-array (list 4 Nb))))
     (dotimes (r 4 ret)
       (dotimes (c Nb)
-	(setf (aref ret r c) (sub-byte (aref state r c)))))))
+	(setf (aref ret r c) (funcall func (aref state r c)))))))
+
+(defun sub-bytes (state)
+  "substitude each byte in state."
+  (map-state state #'sub-byte))
+
+(defparameter *inv-sub-byte-table*
+  #(82 9 106 213 48 54 165 56 191 64 163 158 129 243 215 251 124 227 57 130 155
+    47 255 135 52 142 67 68 196 222 233 203 84 123 148 50 166 194 35 61 238 76
+    149 11 66 250 195 78 8 46 161 102 40 217 36 178 118 91 162 73 109 139 209 37
+    114 248 246 100 134 104 152 22 212 164 92 204 93 101 182 146 108 112 72 80
+    253 237 185 218 94 21 70 87 167 141 157 132 144 216 171 0 140 188 211 10 247
+    228 88 5 184 179 69 6 208 44 30 143 202 63 15 2 193 175 189 3 1 19 138 107 58
+    145 17 65 79 103 220 234 151 242 207 206 240 180 230 115 150 172 116 34 231
+    173 53 133 226 249 55 232 28 117 223 110 71 241 26 113 29 41 197 137 111 183
+    98 14 170 24 190 27 252 86 62 75 198 210 121 32 154 219 192 254 120 205 90
+    244 31 221 168 51 136 7 199 49 177 18 16 89 39 128 236 95 96 81 127 169 25
+    181 74 13 45 229 122 159 147 201 156 239 160 224 59 77 174 42 245 176 200 235
+    187 60 131 83 153 97 23 43 4 126 186 119 214 38 225 105 20 99 85 33 12 125))
 
 ;; :TODO make hash-map
 (defun inv-sub-byte (byte)
-  )
+  (loockup-byte-table byte *inv-sub-byte-table*))
+
+(defun inv-sub-bytes (state)
+  (map-state state #'inv-sub-byte))
 
 ;; #2A((1 2 3 4)
 ;;     (5 6 7 8)
@@ -191,7 +233,7 @@
 
 (defun state-to-output (state)
   (let ((output (make-array (* 4 Nb))))
-    (dotimes (r 4 (bit2int (apply #'concat-bit-array (coerce output 'list))))
+    (dotimes (r 4 (apply #'concat-bit-array (coerce output 'list)))
       (dotimes (c Nb)
 	(setf (aref output (+ r (* 4 c))) (aref state r c))))))
 
@@ -199,25 +241,38 @@
   (flet ((inner-block-to-byte-list (input)
 	   (make-array 16 :initial-contents(loop for i from 0 below 128 by 8
 						 collect (subseq input i (+ i 8))))))
-    (let ((state (input-to-state (inner-block-to-byte-list input)))
-	  (round-keys (key-expansion key)))
+    (let ((state (input-to-state (inner-block-to-byte-list input))) ;; copy input to state
+	  (round-keys (key-expansion key))) ;; get round-keys      
       (setf state (add-round-key state (coerce (subseq round-keys 0 Nb) 'list)))
-      (dotimes (r (- Nr 2))
+      (loop for r from 1 to (1- Nr) do
 	(setf state (add-round-key (mix-columns (shift-rows (sub-bytes state)))
-				   (coerce (subseq round-keys (* (+ r 1) Nb) (* (+ r 2) Nb)) 'list))))
+				   ;; get Nb-round-keys
+				   (coerce (subseq round-keys (* r Nb) (* (1+ r) Nb)) 'list))))      
       (state-to-output (add-round-key (shift-rows (sub-bytes state))
-				      (coerce (subseq round-keys (* (- Nr 1) Nb) (* Nr Nb)) 'list))))))
+				      (coerce (subseq round-keys (* Nr Nb) (* (1+ Nr) Nb)) 'list))))))
 
 (defun decrypt (input key)
   (flet ((inner-block-to-byte-list (input)
 	   (make-array 16 :initial-contents(loop for i from 0 below 128 by 8
 						 collect (subseq input i (+ i 8))))))
     (let ((state (input-to-state (inner-block-to-byte-list input)))
-	  (round-keys (reverse (key-expansion key))))
-      (setf state (add-round-key state (coerce (subseq round-keys 0 Nb) 'list)))
-      (dotimes (r (- Nr 2))
+	  (round-keys (key-expansion key)))
+      (setf state (add-round-key state (coerce (subseq round-keys (* Nr Nb) (* (1+ Nr) Nb)) 'list)))
+      (loop for r downfrom (1- Nr) to 1 do
 	(setf state (inv-mix-columns
 		     (add-round-key (inv-sub-bytes (inv-shift-rows state))
-				    (coerce (subseq round-keys (* (+ r 1) Nb) (* (+ r 2) Nb)) 'list)))))
+				    (coerce (subseq round-keys (* r Nb) (* (+ r 1) Nb)) 'list)))))
       (state-to-output (add-round-key (inv-sub-bytes (inv-shift-rows state))
-				      (coerce (subseq round-keys (* (- Nr 1) Nb) (* Nr Nb)) 'list))))))
+				      (coerce (subseq round-keys 0 Nb) 'list))))))
+
+
+
+(defparameter test-input (int2bit #x3243f6a8885a308d313198a2e0370734 128))
+(defparameter test-key (int2bit #x2b7e151628aed2a6abf7158809cf4f3c 128))
+(defparameter test-output (int2bit #x3925841d02dc09fbdc118597196a0b32 128))
+
+(defun output-hex (state)
+  (let ((ret (make-array (list 4 Nb))))    
+    (dotimes (r 4 ret)
+      (dotimes (c Nb)
+	(setf (aref ret r c) (format nil "~x" (bit2int (aref state r c))))))))
