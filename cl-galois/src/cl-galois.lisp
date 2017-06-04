0aa371357268bf3acd4bab272ad7ff14c50db7c6
4hiziri <meirvg@gmail.com>
2017-06-04 10:12:40 +0900

    reimplement functions: accept integers and return integer

--- cl-galois/src/cl-galois.lisp
+++ cl-galois/src/cl-galois.lisp
@@ -8,17 +8,12 @@
 (defparameter *mx* #b100011011)
 
 @export
-(defun gf-mod (b1 b2 &optional (len 8))
-  (labels ((below-powered (n1 n2 prev-n1)
-	     (if (> n1 n2)
-		 prev-n1
-		 (below-powered (ash n1 1) n2 n1)))
-	   (inner-loop (n1 n2)
-	     (cond ((= (length (int-bit n1)) 9) (int-bit (logxor n1 n2) len))
-		   ((< n1 n2) (int-bit n1 len))
-		   (t (inner-loop (logxor n1 (below-powered n2 n1 n2)) n2)))))
-    (inner-loop (bit-int b1) (bit-int b2))))
+(defun gf-add (num &rest rest)
+  (let ((acc num))
+    (dolist (n rest acc)
+      (setf acc (logxor acc n)))))
 
+@export
 (defun gf-mod (n1 n2)
   (labels ((below-powered (n1 n2 prev-n1)
 	     (if (> n1 n2)
@@ -30,49 +25,45 @@
 		   (t (inner-loop (logxor n1 (below-powered n2 n1 n2)) n2)))))
     (inner-loop n1 n2)))
 
-(defun gf-mult-unfinited (b1 b2)
-  ;; if b2 is 0, cannot calc
-  ;; so simply return 0 as bit-vector
-  (if (reduce (lambda (x y) (and x y)) (map 'list (lambda (x) (= x 0)) b2))
-      (make-array (+ (length b1) (length b2)) :element-type 'bit :initial-element 0)
-      (let ((b2-pos-1 (cl-crypto-util:bit-index-list b2))
-	    (max-len (+ (length b1) (length b2))))
-	(reduce #'bit-xor
-		(mapcar (lambda (x) (cl-crypto-util:bit-shift b1 x))
-			b2-pos-1)))))
-
 (defun gf-mult-unfinited (n1 n2)
   (let ((exponents (cl-crypto-util:bit-indexes n2))
 	(acc 0))
-    (print exponents)
     (dolist (e exponents acc)
       (setf acc (logxor acc (ash n1 e))))))
 
 @export
-(defun gf-mult (b1 b2)
-  (gf-mod (gf-mult-unfinited b1 b2)
-	  (int-bit *mx* 9)
-	  (length b1)))
+(defun gf-mult (num &rest rest)
+  (let ((acc num))
+    (dolist (n rest acc)
+      (setf acc (gf-mod (gf-mult-unfinited acc n) *mx*)))))
 
 @export
-(defun gf-power (bit-array num)
-  (if (= num 0)
-      (int-bit 1 (length bit-array))
-      (let ((acc bit-array))
-	(dotimes (n (1- num) (gf-mod acc (int-bit *mx* 9) (length bit-array)))
-	  (setf acc (gf-mult-unfinited bit-array acc))))))
+(defun gf-power (num exponent)
+  (if (< exponent 1)
+      1
+      (gf-mod (ash num (1- exponent)) *mx*)))
+
+(defun gf-bruteforce-inv (num)
+  "This function calculate gf-inverse of num by brute-force."
+  (if (/= num 0)
+      (loop for i from 1 to 255	    
+	    when (equal (gf-mult i num) 1)
+	      do (return i))
+      0))
 
 @export
-(defun gf-power (bit-array num)
-  (if (= num 0)
-      (int-bit 1 (length bit-array))
-      (gf-mod (bit-shift-pad-0 bit-array (1- num)) #*100011011 (length bit-array))))
-
-@export
-(defun gf-inv (bit-array)
-  (if (equal bit-array #*00000000)
-      #*00000000
-      (loop for i from 1 to 255
-	    for b = (int-bit i 8) then (int-bit i 8)
-	    when (equal (gf-mult b bit-array) #*00000001)
-	      do (return b))))
+(defun gf-inv (num)
+  "This function has lookup vector, which is 256 length"
+  (let ((table #(0 1 141 246 203 82 123 209 232 79 41 192 176 225 229 199 116 180 170 75 153
+    43 96 95 88 63 253 204 255 64 238 178 58 110 90 241 85 77 168 201 193 10 152
+    21 48 68 162 194 44 69 146 108 243 57 102 66 242 53 32 111 119 187 89 25 29
+    254 55 103 45 49 245 105 167 100 171 19 84 37 233 9 237 92 5 202 76 36 135
+    191 24 62 34 240 81 236 97 23 22 94 175 211 73 166 54 67 244 71 145 223 51
+    147 33 59 121 183 151 133 16 181 186 60 182 112 208 6 161 250 129 130 131 126
+    127 128 150 115 190 86 155 158 149 217 247 2 185 164 222 106 50 109 216 138
+    132 114 42 20 159 136 249 220 137 154 251 124 46 195 143 184 101 72 38 200 18
+    74 206 231 210 98 12 224 31 239 17 117 120 113 165 142 118 61 189 188 134 87
+    11 40 47 163 218 212 228 15 169 39 83 4 27 252 172 230 122 7 174 99 197 219
+    226 234 148 139 196 213 157 248 144 107 177 13 214 235 198 14 207 173 8 78
+    215 227 93 80 30 179 91 35 56 52 104 70 3 140 221 156 125 160 205 26 65 28)))
+    (aref table num)))
--- cl-galois/t/cl-galois.lisp
+++ cl-galois/t/cl-galois.lisp
@@ -11,6 +11,16 @@
 
 (diag "Run cl-galois test")
 
+(subtest "gf-add"
+  (let ((test-val1 #x57)
+	(test-val2 #x83)
+	(result #xd4))
+    (is (cl-galois:gf-add test-val1 test-val2)
+	result)
+    (is (cl-galois:gf-add test-val1 test-val2 result)
+	0)
+    (ok (<= (integer-length (cl-galois:gf-add test-val1 test-val2)) 8))))
+
 (subtest "gf-mod"
   (let ((test-val 11129)
 	(ret 193))
@@ -23,5 +33,33 @@
 	(res #b10101101111001))
     (ok (equalp (print (cl-galois::gf-mult-unfinited test-val1 test-val2))
 		res))))
+
+(subtest "gf-mult"
+  (let ((test1 #x57)
+	(test2 #x83)
+	(result #xc1))
+    (is (cl-galois:gf-mult test1 test2)
+	result)
+    (ok (<= (integer-length (cl-galois:gf-mult test1 test2)) 8))))
+
+(subtest "gf-power"
+  (let ((test 2)
+	(num1 0)
+	(num2 1)
+	(num3 2)
+	(res1 1)
+	(res2 2)
+	(res3 4))
+    (is (cl-galois:gf-power test num1)
+	res1)
+    (is (cl-galois:gf-power test num2)
+	res2)
+    (is (cl-galois:gf-power test num3)
+	res3)))
+
+(subtest "gf-inv"
+  (let ((test 123))
+    (ok (= (cl-galois:gf-mult (cl-galois:gf-inv test) test)
+	   1))))
 
 (finalize)
